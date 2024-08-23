d7edff76606a3fcac098370a485bbd868328e9ca
ManliestBen <benjamintmanley@gmail.com>
2024-08-23 13:42:17 -0500

    implement delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // npm modules
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
 // pages
 import NewCar from './pages/NewCar/NewCar'
@@ -14,16 +14,30 @@ import './App.css'
 
 function App() {
   const [cars, setCars] = useState([])
+  const navigate = useNavigate()
 
   const handleAddCar = carData => {
     setCars([...cars, carData])
+    navigate('/cars')
+  }
+
+  const handleRemoveCar = carId => {
+    setCars(cars.filter(car => car.id !== carId))
   }
 
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path='/cars' element={<Cars cars={cars} />}/>
+        <Route 
+          path='/cars' 
+          element={
+            <Cars 
+              cars={cars} 
+              handleRemoveCar={handleRemoveCar} 
+            />
+          }
+        />
         <Route 
           path='/cars/new' 
           element={<NewCar handleAddCar={handleAddCar} />} 
--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,19 +1,14 @@
-const Cars = ({ cars }) => {
+const Cars = ({ cars, handleRemoveCar }) => {
   return (
     <>
       <h1>Car List</h1>
       <div className="car-container">
         {cars.map(car =>
           <div key={car.id} className="car-card">
-            <h2>
-              {car.year}
-            </h2>
-            <h2>
-              {car.make}
-            </h2>
-            <h2>
-              {car.model}
-            </h2>
+            <h2>{car.year}</h2>
+            <h2>{car.make}</h2>
+            <h2>{car.model}</h2>
+            <button onClick={() => handleRemoveCar(car.id)}>🗑️</button>
           </div>
         )}
       </div>
