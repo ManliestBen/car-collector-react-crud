ebbafe3bd62e0774730ac519bc383faade20b028
ManliestBen <benjamintmanley@gmail.com>
2024-08-23 13:04:22 -0500

    add state to new car form

--- src/pages/NewCar/NewCar.jsx
+++ src/pages/NewCar/NewCar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import { v4 as uuidv4 } from 'uuid'
 
 const NewCar = () => {
   const [formData, setFormData] = useState({
@@ -7,15 +7,66 @@ const NewCar = () => {
     model: '',
     year: 0,
     color: '',
-    mileage: 0
+    mileage: 0,
+    id: uuidv4().replaceAll('-', '')
   })
+
+  const handleChange = e => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
   return (
     <form>
-      <input type="text" name="make" placeholder="Make" />
-      <input type="text" name="model" placeholder="Model" />
-      <input type="text" name="color" placeholder="Color" />
-      <input type="number" name="year" placeholder="Year"/>
-      <input type="number" name="mileage" placeholder="Mileage"/>
+      <label>
+        Make
+        <input 
+          type="text" 
+          name="make" 
+          placeholder="Make"
+          value={formData.make}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Model
+        <input 
+          type="text" 
+          name="model" 
+          placeholder="Model" 
+          value={formData.model}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Color
+        <input 
+          type="text" 
+          name="color" 
+          placeholder="Color" 
+          value={formData.color}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Year
+        <input 
+          type="number" 
+          name="year" 
+          placeholder="Year"
+          value={formData.year}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Mileage
+        <input 
+        type="number" 
+          name="mileage" 
+          placeholder="Mileage"
+          value={formData.mileage}
+          onChange={handleChange}
+        />
+      </label>
       <button type="submit">Add Car 🚗</button>
     </form>
   )
